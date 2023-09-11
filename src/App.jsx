9bab5f087ff4ccca3236cdd1b094b8abcd63b200
ictrumon <rumon.bd247@gmail.com>
2023-09-12 00:45:54 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import './App.css'
+import Blogs from './Components/Blogs/Blogs'
+import Bookmarks from './Components/Bookmarks/Bookmarks'
+import Header from './Components/Header/Header'
+
+  function App() {
+  const [bookmarks, setBookmarks] = useState([]);
+  const [readingTime, setReadingTime] = useState(0)
+
+  const handleAddToBookmark = blog => {
+  const newBookmarks = [...bookmarks, blog];
+  setBookmarks(newBookmarks);
+}
+
+const handleMarkAsRead = (id, time) => {
+  const newReadingTime = readingTime + time;
+  setReadingTime(newReadingTime);
+
+  // remove the read blog from 
+  // console.log('remove bookmark,', id);
+  const remainingBookmarks = bookmarks.filter(bookmark => bookmark.id !== id);
+  setBookmarks(remainingBookmarks)
+
+}
+  return (
+    <>
+      <Header></Header>
+      <div className='flex max-w-7xl mx-auto'>
+      <Blogs handleAddToBookmark={handleAddToBookmark} handleMarkAsRead={handleMarkAsRead}></Blogs>
+      <Bookmarks bookmarks={bookmarks} readingTime={readingTime}></Bookmarks>
+      </div>
+    </>
+  )
+}
+
+export default App
